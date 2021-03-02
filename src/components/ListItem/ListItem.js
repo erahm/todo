27c9aa31daa06b896e3a85def8cb1392aac42321
Erich Rahm <erahm84@gmail.com>
2021-03-01 20:46:27 -0700

    updates todo list component to build list items dynamically

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -4,11 +4,29 @@ import "./list-item.scss";
 export default (props) => {
   const {id, task, complete, dueDate} = props;
 
-  console.log("props", props);
+  const [checked, setChecked] = useState(complete);
+
+  const handleChange = (event) => {
+    const target = event.currentTarget;
+
+
+    if (target.checked) {
+      setChecked(true)
+    }
+    else {
+      setChecked(false)
+    }
+  }
 
   return (
     <li>
-      <input type="checkbox" key={`task-${id}`} name={`task-${id}`} checked={ complete ? checked : ""} />
+      <input
+        type="checkbox"
+        key={`task-${id}`}
+        name={`task-${id}`}
+        onChange={(event) => handleChange(event)}
+        checked={ checked ? "checked" : ""}
+      />
       <span>{ task }</span>
       <span>{ dueDate }</span>
     </li>
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,18 +2,33 @@ import React from "react";
 import ListItem from "../ListItem";
 import "./todo-list.scss";
 
-const fakeData = {
-  id: 1,
-  task: "do some stuff",
-  complete: false,
-  dueDate: null
-};
+const fakeData = [
+  {
+    id: 1,
+    task: "do some stuff",
+    complete: false,
+    dueDate: null
+  },
+  {
+    id: 2,
+    task: "do some other stuff",
+    complete: true,
+    dueDate: null
+  }
+];
 
 export default () => {
+
+  const renderItems = (items) => {
+    return items.map((item) => {
+      return <ListItem key={`item-${item.id}`} {...item} />
+    })
+  }
+
   return (
     <div className="app-wrapper">
       <ul>
-        <ListItem {...fakeData} />
+        { renderItems(fakeData) }
       </ul>
     </div>
   )
